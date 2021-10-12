fdf55f53640aca64bd01e7842524241f087b8753
nandha_the_ken <nandha@the-ken.com>
2021-10-12 08:46:26 +0530

    Removed unused variable

--- app/src/main/java/io/nandha/userprofiles/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/io/nandha/userprofiles/viewmodel/MainActivityViewModel.kt
@@ -8,15 +8,11 @@ import io.nandha.userprofiles.model.Api
 import io.nandha.userprofiles.model.Repository
 import io.nandha.userprofiles.model.User
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.count
 
 class MainActivityViewModel : ViewModel() {
-    private var users: Flow<PagingData<User>>? = null
     private val repository: Repository = Repository(Api.create())
 
     fun loadUser(): Flow<PagingData<User>> {
-        val newResult = repository.getUsers().cachedIn(viewModelScope)
-        users = newResult
-        return newResult
+        return repository.getUsers().cachedIn(viewModelScope)
     }
 }
